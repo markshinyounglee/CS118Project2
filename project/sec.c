681d4fa00e9b30dd1c88c9642651ccb67926e251
Shinyoung Lee (Mark) <markshinyounglee@gmail.com>
2024-11-30 17:51:21 -0800

    fixed up to security handshakes. Now we need to troubleshoot DATA_STATE

--- project/sec.c
+++ project/sec.c
@@ -75,22 +75,21 @@ ssize_t input_sec(uint8_t* buf, size_t max_length) {
         // certificate
         // load_certificate() already called
         // size: cert_size, buffer: certificate
-        uint8_t* cert_buf = (uint8_t*) malloc(cert_size + 3);
-        int certificate_size = TLV_maker(cert_buf, CERTIFICATE, cert_size, certificate);
+        // already packaged in TLV format
 
         // nonce signature 
-        uint8_t* nonce_signature = (int8_t*) malloc(255);
+        uint8_t* nonce_signature = (int8_t*) malloc(255); // usually signature is around 70-72 bytes long
         int sign_size = sign(peer_nonce, NONCE_SIZE, nonce_signature); 
         uint8_t* nonce_sign_buf = (uint8_t*) malloc(sign_size + 3);
         fprintf(stderr, "signature size: %d\n", sign_size);
         int signature_size = TLV_maker(nonce_sign_buf, NONCE_SIGNATURE_SERVER_HELLO, sign_size, nonce_signature);
         
         // put everything in load before writing to buffer
-        int total_size = nonce_size + certificate_size + signature_size;
+        int total_size = nonce_size + cert_size + signature_size;
         uint8_t* load = (uint8_t*) malloc(total_size);
         memcpy(load, nonce_buf, nonce_size);
-        memcpy(load+nonce_size, cert_buf, certificate_size);
-        memcpy(load+nonce_size+certificate_size, nonce_sign_buf, signature_size);
+        memcpy(load+nonce_size, certificate, cert_size);
+        memcpy(load+nonce_size+cert_size, nonce_sign_buf, signature_size);
     
         // write to buf
         int load_size = TLV_maker(buf, SERVER_HELLO, total_size, load);
@@ -98,7 +97,6 @@ ssize_t input_sec(uint8_t* buf, size_t max_length) {
         print_tlv(buf, load_size);
 
         free(nonce_buf);
-        free(cert_buf);
         free(nonce_sign_buf);
         free(load);
 
@@ -110,7 +108,8 @@ ssize_t input_sec(uint8_t* buf, size_t max_length) {
 
         /* Insert Key Exchange Request sending logic here */
         // cert_size and certificate are global variables
-        derive_self_signed_certificate();
+        // called already
+        // derive_self_signed_certificate();
         // my own code commented out:
         // uint8_t raw_key_buf[0];
         // int key_size = TLV_maker(raw_key_buf, PUBLIC_KEY, pub_key_size, public_key);
@@ -236,7 +235,7 @@ void output_sec(uint8_t* buf, size_t length) {
 
         /* Insert Client Hello receiving logic here */
         // put client nonce into peer_nonce
-        int nonce_loc = find_location(buf, state_sec, NONCE_SIZE);
+        int nonce_loc = find_location(buf, state_sec, NONCE_CLIENT_HELLO);
 
         memcpy(peer_nonce, &buf[nonce_loc], NONCE_SIZE);
 
@@ -250,6 +249,8 @@ void output_sec(uint8_t* buf, size_t length) {
         print("RECV SERVER HELLO");
 
         /* Insert Server Hello receiving logic here */
+        // print the buffer content
+        print_tlv(buf, get_size(buf, 3));
         int peer_sign_loc = find_location(buf, state_sec, SIGNATURE);
         uint16_t peer_sign_size = get_size(buf, peer_sign_loc);
         int peer_pubkey_loc = find_location(buf, state_sec, PUBLIC_KEY);
@@ -274,7 +275,7 @@ void output_sec(uint8_t* buf, size_t length) {
         }
 
         // 2. verify that client nonce was signed by the server
-        int peer_nonce_sign_loc = find_location(buf, state_sec, NONCE_SERVER_HELLO);
+        int peer_nonce_sign_loc = find_location(buf, state_sec, NONCE_SIGNATURE_SERVER_HELLO);
         uint16_t peer_nonce_sign_size = get_size(buf, peer_nonce_sign_loc);
         status = verify(
             nonce, // client nonce
@@ -290,8 +291,8 @@ void output_sec(uint8_t* buf, size_t length) {
         }
 
         // put server nonce into peer_nonce
-        int peer_nonce_loc = find_location(buf, state_sec, NONCE_SERVER_HELLO);
-        memcpy(&buf[peer_nonce_loc], peer_nonce, NONCE_SIZE);
+        int server_nonce_loc = find_location(buf, state_sec, NONCE_SERVER_HELLO);
+        memcpy(peer_nonce, &buf[server_nonce_loc], NONCE_SIZE);
 
         // generate ENC and MAC keys using HKDF
         // do this once you load the peer public key
@@ -334,11 +335,11 @@ void output_sec(uint8_t* buf, size_t length) {
         int client_signed_nonce_loc = find_location(buf, state_sec, NONCE_SIGNATURE_KEY_EXCHANGE_REQUEST);
         int client_signed_nonce_size = get_size(buf, client_signed_nonce_loc);
         status = verify(
-            nonce, 
+            nonce,  // raw nonce of the server
             NONCE_SIZE, 
-            &buf[client_signed_nonce_loc], 
+            &buf[client_signed_nonce_loc],  // client-signed nonce
             client_signed_nonce_size,
-            ec_peer_public_key
+            ec_peer_public_key // authority: client public key
         );
         if (status != 1)
         {
@@ -420,10 +421,11 @@ void output_sec(uint8_t* buf, size_t length) {
 
 uint32_t TLV_maker(uint8_t* buf, uint8_t type, uint16_t length, uint8_t* value)
 {   
-    fprintf(stderr, "buf has %p\n", buf);
+    //# fprintf(stderr, "buf has %p\n", buf);
     if(buf == NULL)
     {
-        exit(234);
+        fprintf(stderr, "buffer empty\n");
+        exit(139); // SEGSEV (segfault)
     }
     print("TLV maker entered");
     uint32_t load_size = TYPE_SIZE + LENGTH_SIZE + length;  // T (1B) + L (2B) + V (NONCE_SIZE)
@@ -432,13 +434,13 @@ uint32_t TLV_maker(uint8_t* buf, uint8_t type, uint16_t length, uint8_t* value)
     // how to copy int into byte array: https://community.intel.com/t5/Intel-Integrated-Performance/Convert-an-int-to-a-byte-array/td-p/888464
     uint16_t len = htons(length);
     memcpy(load+1, &len, sizeof(len)); // length in network should be big-endian
-    print("length copied");
+    //# print("length copied");
     memcpy(load+3, value, length); // value is arbitary length
-    print("value copied");
-    fprintf(stderr, "buf: %p, load: %p, load_size: %u\n", buf, load, load_size);
-    fprintf(stderr, "bufsize: %lu\n", sizeof(buf));
+    //# print("value copied");
+    //# fprintf(stderr, "buf: %p, load: %p, load_size: %u\n", buf, load, load_size);
+    //# fprintf(stderr, "bufsize: %lu\n", sizeof(buf));
     memcpy(buf, load, load_size);
-    print("load copied");
+    //# print("load copied");
     return load_size;
 }
 
@@ -458,38 +460,48 @@ int find_location(uint8_t* data, int state_sec, uint8_t type)
     int total_size = 0;
     while(1)
     {
-        int currlen = locator - data + 3;
+        fprintf(stderr, "locator: %x\n", *locator);
+        int currlen = locator - data;
         if(*locator == type)
         {
-            return currlen; // return the beginning index of field 
+            return currlen+3; // return the beginning index of field 
         }
         else if (total_size != 0 && currlen >= total_size) // maximum exceeded
         {
             break;
         }
-        if(*locator == SERVER_HELLO 
+        else if(*locator == SERVER_HELLO 
             || *locator == CLIENT_HELLO
-            || *locator == KEY_EXCHANGE_REQUEST 
+            || *locator == KEY_EXCHANGE_REQUEST
             || *locator == DATA)
         {
-            total_size = 3 + get_size(data, currlen);
+            total_size = 3 + get_size(data, currlen+3);
+            fprintf(stderr, "total_size: %d\n", total_size);
             locator += 3; // jump to the payload
+        }
+        else if(*locator == CERTIFICATE || *locator == FINISHED)
+        {
+            fprintf(stderr, "certificate or finished encountered\n");
+            locator += 3;
         }
         else
         {
-            int jump = get_size(data, currlen);
-            locator += jump;
+            int jump = get_size(data, currlen+3);
+            fprintf(stderr, "jumping %d bytes\n", jump);
+            locator += (jump + 3);
         }
     }
 
-    fprintf(stderr, "Finding location failed for %d in %d\n", state_sec, type);
-    return 0;
+    fprintf(stderr, "Finding location failed for %d in %x\n", state_sec, type);
+    exit(4);
 }
 // get size of the field
 // location is found by find_location
 uint16_t get_size(uint8_t* data, int bufloc) 
 {
-    return ntohs(*((uint16_t*) &data[bufloc-2])); // convert to little endian
+    uint16_t retval = ntohs(*((uint16_t*) &data[bufloc-2]));
+    fprintf(stderr, "field size: %u\n", retval);
+    return retval; // convert to little endian
 }
 // compute the max length of plain text that could be read at a time
 uint16_t plaintxt_length(uint16_t payload_length)
